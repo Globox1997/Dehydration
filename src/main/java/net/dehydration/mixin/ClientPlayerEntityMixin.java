7ea2aab8eb893dd01055361e21a8db23e47d524f
Globox1997 <mitschke11@web.de>
2021-04-09 16:40:39 +0200

    cant sprint while very thirsty

---
+++ src/main/java/net/dehydration/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,35 @@
+package net.dehydration.mixin;
+
+import com.mojang.authlib.GameProfile;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.fabricmc.api.Environment;
+import net.dehydration.access.ThristManagerAccess;
+import net.dehydration.thirst.ThirstManager;
+import net.fabricmc.api.EnvType;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+
+@Environment(EnvType.CLIENT)
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
+
+    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
+        super(world, profile);
+    }
+
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;setSprinting(Z)V", shift = Shift.AFTER))
+    public void tickMovementMixin(CallbackInfo info) {
+        ThirstManager thirstManager = ((ThristManagerAccess) this).getThirstManager(this);
+        if (thirstManager.getThirstLevel() < 6) {
+            this.setSprinting(false);
+        }
+    }
+
+}
