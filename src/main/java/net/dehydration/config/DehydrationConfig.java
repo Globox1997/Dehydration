63c6c5b2bfe6fd4f5d75278e7640d5902d8c77e1
Erik <51373236+Gappysona@users.noreply.github.com>
2021-04-11 21:12:11 +0100

    Updated comments to be as simple and easy as possible

--- src/main/java/net/dehydration/config/DehydrationConfig.java
+++ src/main/java/net/dehydration/config/DehydrationConfig.java
@@ -12,41 +12,44 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config.Gui.Background("minecraft:textures/block/stone.png")
 public class DehydrationConfig implements ConfigData {
   // @ConfigEntry.Category("hydration_settings")
+  @Comment("Defines the damage taken every 4 seconds")
   public float thirst_damage = 1.0F;
-  @Comment("Defines hydrating rate, bigger variable = slower dehydrate")
+  @Comment("Defines speed of dehydration, bigger value = slower depletion")
   public float hydrating_factor = 1.5F;
-  @Comment("Defines thirst effect draining rate, smaller variable = less draining")
-  public float thirst_effect_factor = 0.05F;
+  @Comment("Defines the rate of the thirst effect's drain, smaller variable = less draining")
+  public float thirst_effect_factor = 0.033F;
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int potion_thirst_quench = 2;
-  @Comment("0.25F = 25%")
+  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
   public float potion_bad_thirst_chance = 0.25F;
-  @Comment("20ticks = 1second")
+  @Comment("Counted in ticks, 20 ticks = 1 second")
   public int potion_bad_thirst_duration = 300;
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int milk_thirst_quench = 8;
-  @Comment("0.4F = 40%")
+  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
   public float milk_thirst_chance = 0.4F;
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int stew_thirst_quench = 3;
-  @Comment("Applies only for food tagged with: hydrating_food")
+  @Comment("Applies to only food tagged with: #hydrating_food")
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int food_thirst_quench = 1;
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int flask_thirst_quench = 4;
+  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
   public float flask_dirty_thirst_chance = 0.75F;
-  @Comment("20ticks = 1second")
+  @Comment("Counted in ticks, 20 ticks = 1 second")
   public int flask_dirty_thirst_duration = 500;
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int water_souce_quench = 1;
-  @Comment("0.75F = 75%")
+  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
   public float water_sip_thirst_chance = 0.75F;
-  @Comment("20ticks = 1second")
+  @Comment("Counted in ticks, 20 ticks = 1 second")
   public int water_sip_thirst_duration = 600;
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int sleep_thirst_consumption = 4;
   @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
   public int sleep_hunger_consumption = 2;
+  @Comment("Enables alternate textures by the lead texture artist")
   public boolean other_droplet_texture = false;
   @Comment("Players listed here won't have thirst")
   public List<String> excluded_names = new ArrayList<>();
