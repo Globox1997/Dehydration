6a860fe57efb1d43d0180d3f09934642427bb7aa
Globox1997 <mitschke11@web.de>
2021-04-11 15:52:38 +0200

    removed dehydration effect

--- src/main/java/net/dehydration/init/TagInit.java
+++ src/main/java/net/dehydration/init/TagInit.java
@@ -7,7 +7,6 @@ import net.minecraft.util.Identifier;
 
 public class TagInit {
 
-  public static final Tag<Item> ALLOWED_ARMOR = TagRegistry.item(new Identifier("dehydration", "allowed_armor"));
   public static final Tag<Item> HYDRATING_FOOD = TagRegistry.item(new Identifier("dehydration", "hydrating_food"));
   public static final Tag<Item> HYDRATING_STEW = TagRegistry.item(new Identifier("dehydration", "hydrating_stew"));
   public static final Tag<Item> HYDRATING_DRINKS = TagRegistry.item(new Identifier("dehydration", "hydrating_drinks"));
