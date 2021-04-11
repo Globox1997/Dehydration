b6f29ba2b79aeada261b781a46968a0d5e021f22
Globox1997 <mitschke11@web.de>
2021-04-11 14:50:07 +0200

    added bad potion thirst chance

--- src/main/java/net/dehydration/mixin/PotionItemMixin.java
+++ src/main/java/net/dehydration/mixin/PotionItemMixin.java
@@ -7,11 +7,16 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import net.dehydration.access.ThristManagerAccess;
 import net.dehydration.init.ConfigInit;
+import net.dehydration.init.EffectInit;
 import net.dehydration.thirst.ThirstManager;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PotionItem;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
 import net.minecraft.world.World;
 
 @Mixin(PotionItem.class)
@@ -22,9 +27,25 @@ public abstract class PotionItemMixin {
       CallbackInfoReturnable<ItemStack> info) {
     if (user instanceof PlayerEntity) {
       PlayerEntity player = (PlayerEntity) user;
+      Potion potion = PotionUtil.getPotion(stack);
+      if (this.isBadPotion(potion) && world.random.nextFloat() >= ConfigInit.CONFIG.potion_bad_thirst_chance) {
+        player.addStatusEffect(new StatusEffectInstance(EffectInit.THIRST, ConfigInit.CONFIG.potion_bad_thirst_duration,
+            0, false, false, true));
+      }
       ThirstManager thirstManager = ((ThristManagerAccess) player).getThirstManager(player);
       thirstManager.add(ConfigInit.CONFIG.potion_thirst_quench);
     }
+  }
+
+  private boolean isBadPotion(Potion potion) {
+    if (potion == Potions.WATER || potion == Potions.AWKWARD || potion == Potions.THICK || potion == Potions.HARMING
+        || potion == Potions.LONG_POISON || potion == Potions.LONG_SLOWNESS || potion == Potions.LONG_WEAKNESS
+        || potion == Potions.MUNDANE || potion == Potions.POISON || potion == Potions.SLOWNESS
+        || potion == Potions.STRONG_HARMING || potion == Potions.STRONG_POISON || potion == Potions.STRONG_SLOWNESS
+        || potion == Potions.WEAKNESS) {
+      return true;
+    } else
+      return false;
   }
 
 }
