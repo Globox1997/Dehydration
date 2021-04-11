1e75e033c11990c9c2cc23538c1cc36fbf2c9b23
Globox1997 <mitschke11@web.de>
2021-04-11 14:33:44 +0200

    added water sip sounds

--- src/main/java/net/dehydration/init/SoundInit.java
+++ src/main/java/net/dehydration/init/SoundInit.java
@@ -7,10 +7,13 @@ import net.minecraft.util.registry.Registry;
 public class SoundInit {
 
   public static final Identifier FILL_FLASK = new Identifier("dehydration:fill_flask");
+  public static final Identifier WATER_SIP = new Identifier("dehydration:water_sip");
+  public static SoundEvent WATER_SIP_EVENT = new SoundEvent(WATER_SIP);
   public static SoundEvent FILL_FLASK_EVENT = new SoundEvent(FILL_FLASK);
 
   public static void init() {
     Registry.register(Registry.SOUND_EVENT, FILL_FLASK, FILL_FLASK_EVENT);
+    Registry.register(Registry.SOUND_EVENT, WATER_SIP, WATER_SIP_EVENT);
   }
 
 }
