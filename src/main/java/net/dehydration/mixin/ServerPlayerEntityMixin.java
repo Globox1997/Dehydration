def98790b56729746c67ce87496efa9ba1f5ace3
Globox1997 <mitschke11@web.de>
2021-06-24 10:36:47 +0200

    fixed render issue

--- src/main/java/net/dehydration/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/dehydration/mixin/ServerPlayerEntityMixin.java
@@ -2,10 +2,12 @@ package net.dehydration.mixin;
 
 import com.mojang.authlib.GameProfile;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.netty.buffer.Unpooled;
 
@@ -16,40 +18,52 @@ import net.dehydration.init.ConfigInit;
 import net.dehydration.network.ThirstUpdateS2CPacket;
 import net.dehydration.thirst.ThirstManager;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
-  ThirstManager thirstManager = ((ThristManagerAccess) this).getThirstManager(this);
-  private int syncedThirstLevel = -99999999;
+    ThirstManager thirstManager = ((ThristManagerAccess) this).getThirstManager(this);
+    private int syncedThirstLevel = -99999999;
 
-  public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
-    super(world, pos, yaw, profile);
-  }
-
-  @Inject(method = "playerTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;tick()V", shift = Shift.AFTER))
-  public void playerTickMixin(CallbackInfo info) {
-    if (this.syncedThirstLevel != this.thirstManager.getThirstLevel()
-        && !ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
-      PacketByteBuf data = new PacketByteBuf(Unpooled.buffer());
-      data.writeIntArray(new int[] { this.getId(), thirstManager.getThirstLevel() });
-      ServerPlayNetworking.send((ServerPlayerEntity) (Object) this, ThirstUpdateS2CPacket.THIRST_UPDATE, data);
-      this.syncedThirstLevel = thirstManager.getThirstLevel();
+    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
+        super(world, pos, yaw, profile);
     }
-  }
 
-  @Inject(method = "Lnet/minecraft/server/network/ServerPlayerEntity;copyFrom(Lnet/minecraft/server/network/ServerPlayerEntity;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;clone(Lnet/minecraft/entity/player/PlayerInventory;)V", shift = Shift.AFTER))
-  public void copyFromMixinOne(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo info) {
-    this.thirstManager = ((ThristManagerAccess) oldPlayer).getThirstManager(oldPlayer);
-  }
+    @Inject(method = "playerTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;tick()V", shift = Shift.AFTER))
+    public void playerTickMixin(CallbackInfo info) {
+        if (this.syncedThirstLevel != this.thirstManager.getThirstLevel() && !ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
+            PacketByteBuf data = new PacketByteBuf(Unpooled.buffer());
+            data.writeIntArray(new int[] { this.getId(), thirstManager.getThirstLevel() });
+            ServerPlayNetworking.send((ServerPlayerEntity) (Object) this, ThirstUpdateS2CPacket.THIRST_UPDATE, data);
+            this.syncedThirstLevel = thirstManager.getThirstLevel();
+        }
+    }
 
-  @Inject(method = "Lnet/minecraft/server/network/ServerPlayerEntity;copyFrom(Lnet/minecraft/server/network/ServerPlayerEntity;Z)V", at = @At(value = "TAIL"))
-  public void copyFromMixinTwo(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo info) {
-    this.syncedThirstLevel = -1;
-  }
+    @Inject(method = "Lnet/minecraft/server/network/ServerPlayerEntity;copyFrom(Lnet/minecraft/server/network/ServerPlayerEntity;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;clone(Lnet/minecraft/entity/player/PlayerInventory;)V", shift = Shift.AFTER))
+    public void copyFromMixinOne(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo info) {
+        this.thirstManager = ((ThristManagerAccess) oldPlayer).getThirstManager(oldPlayer);
+    }
+
+    @Inject(method = "Lnet/minecraft/server/network/ServerPlayerEntity;copyFrom(Lnet/minecraft/server/network/ServerPlayerEntity;Z)V", at = @At(value = "TAIL"))
+    public void copyFromMixinTwo(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo info) {
+        this.syncedThirstLevel = -1;
+    }
+
+    @Inject(method = "teleport", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;setWorld(Lnet/minecraft/server/world/ServerWorld;)V"))
+    void teleportFix(ServerWorld targetWorld, double x, double y, double z, float yaw, float pitch, CallbackInfo info) {
+        this.syncedThirstLevel = -1;
+    }
+
+    @Nullable
+    @Inject(method = "moveToWorld", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerPlayerEntity;syncedFoodLevel:I", ordinal = 0))
+    private void moveToWorldMixin(ServerWorld destination, CallbackInfoReturnable<Entity> info) {
+        this.syncedThirstLevel = -1;
+    }
 
 }
