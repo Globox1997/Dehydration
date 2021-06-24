8aee943ce2230b9c6984922741dbe1256d0ed5be
Globox1997 <mitschke11@web.de>
2021-06-24 10:36:22 +0200

    added nether config

--- src/main/java/net/dehydration/config/DehydrationConfig.java
+++ src/main/java/net/dehydration/config/DehydrationConfig.java
@@ -11,48 +11,51 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config(name = "dehydration")
 @Config.Gui.Background("minecraft:textures/block/stone.png")
 public class DehydrationConfig implements ConfigData {
-  // @ConfigEntry.Category("hydration_settings")
-  @Comment("Defines the damage taken every 4 seconds")
-  public float thirst_damage = 1.0F;
-  @Comment("Defines speed of dehydration, bigger value = slower depletion")
-  public float hydrating_factor = 1.5F;
-  @Comment("Defines the rate of the thirst effect's drain, smaller variable = less draining")
-  public float thirst_effect_factor = 0.033F;
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int potion_thirst_quench = 2;
-  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
-  public float potion_bad_thirst_chance = 0.25F;
-  @Comment("Counted in ticks, 20 ticks = 1 second")
-  public int potion_bad_thirst_duration = 300;
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int milk_thirst_quench = 8;
-  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
-  public float milk_thirst_chance = 0.4F;
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int stew_thirst_quench = 3;
-  @Comment("Applies to only food tagged with: #hydrating_food")
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int food_thirst_quench = 1;
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int flask_thirst_quench = 4;
-  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
-  public float flask_dirty_thirst_chance = 0.75F;
-  @Comment("Counted in ticks, 20 ticks = 1 second")
-  public int flask_dirty_thirst_duration = 500;
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int water_souce_quench = 1;
-  @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
-  public float water_sip_thirst_chance = 0.75F;
-  @Comment("Counted in ticks, 20 ticks = 1 second")
-  public int water_sip_thirst_duration = 600;
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int sleep_thirst_consumption = 4;
-  @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-  public int sleep_hunger_consumption = 2;
-  @Comment("in ticks")
-  public int water_boiling_time = 100;
-  @Comment("Enables alternate textures by the lead texture artist")
-  public boolean other_droplet_texture = false;
-  @Comment("Players listed here won't have thirst")
-  public List<String> excluded_names = new ArrayList<>();
+    // @ConfigEntry.Category("hydration_settings")
+    @Comment("Defines the damage taken every 4 seconds")
+    public float thirst_damage = 1.0F;
+    @Comment("Defines speed of dehydration, bigger value = slower depletion")
+    public float hydrating_factor = 1.5F;
+    @Comment("Defines the rate of the thirst effect's drain, smaller variable = less draining")
+    public float thirst_effect_factor = 0.033F;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int potion_thirst_quench = 2;
+    @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
+    public float potion_bad_thirst_chance = 0.25F;
+    @Comment("Counted in ticks, 20 ticks = 1 second")
+    public int potion_bad_thirst_duration = 300;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int milk_thirst_quench = 8;
+    @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
+    public float milk_thirst_chance = 0.4F;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int stew_thirst_quench = 3;
+    @Comment("Applies to only food tagged with: #hydrating_food")
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int food_thirst_quench = 1;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int flask_thirst_quench = 4;
+    @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
+    public float flask_dirty_thirst_chance = 0.75F;
+    @Comment("Counted in ticks, 20 ticks = 1 second")
+    public int flask_dirty_thirst_duration = 500;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int water_souce_quench = 1;
+    @Comment("1.0F = 100%, 0.5F = 50%, 0.0F = 0%")
+    public float water_sip_thirst_chance = 0.75F;
+    @Comment("Counted in ticks, 20 ticks = 1 second")
+    public int water_sip_thirst_duration = 600;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int sleep_thirst_consumption = 4;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
+    public int sleep_hunger_consumption = 2;
+    @Comment("in ticks")
+    public int water_boiling_time = 100;
+    public boolean harder_nether = false;
+    @Comment("1.3 = 30% more thirst")
+    public float nether_factor = 1.3F;
+    @Comment("Enables alternate textures by the lead texture artist")
+    public boolean other_droplet_texture = false;
+    @Comment("Players listed here won't have thirst")
+    public List<String> excluded_names = new ArrayList<>();
 }
--- src/main/java/net/dehydration/mixin/PlayerEntityMixin.java
+++ src/main/java/net/dehydration/mixin/PlayerEntityMixin.java
@@ -21,71 +21,72 @@ import net.minecraft.world.World;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements ThristManagerAccess {
-  private ThirstManager thirstManager = new ThirstManager();
+    private ThirstManager thirstManager = new ThirstManager();
 
-  @Override
-  public ThirstManager getThirstManager(PlayerEntity player) {
-    return this.thirstManager;
-  }
-
-  @Shadow
-  protected HungerManager hungerManager = new HungerManager();
-  @Shadow
-  private int sleepTimer;
-
-  public PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-    super(entityType, world);
-  }
-
-  @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;update(Lnet/minecraft/entity/player/PlayerEntity;)V", shift = Shift.AFTER))
-  private void tickMixin(CallbackInfo info) {
-    if (!ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
-      this.thirstManager.update((PlayerEntity) (Object) this);
-    }
-  }
-
-  @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;tickMovement()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;updateItems()V", shift = Shift.BEFORE))
-  private void tickMovementMixin(CallbackInfo info) {
-    if (this.world.getDifficulty() == Difficulty.PEACEFUL
-        && this.world.getGameRules().getBoolean(GameRules.NATURAL_REGENERATION)
-        && !ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
-      PlayerEntity player = (PlayerEntity) (Object) this;
-      this.thirstManager.update(player);
-      if (this.thirstManager.isNotFull() && this.age % 10 == 0) {
-        this.thirstManager.setThirstLevel(this.thirstManager.getThirstLevel() + 1);
-      }
-    }
-  }
-
-  @Inject(method = "readCustomDataFromNbt", at = @At(value = "TAIL"))
-  private void readCustomDataFromTagMixin(NbtCompound tag, CallbackInfo info) {
-    this.thirstManager.readNbt(tag);
-  }
-
-  @Inject(method = "writeCustomDataToNbt", at = @At(value = "TAIL"))
-  private void writeCustomDataToTagMixin(NbtCompound tag, CallbackInfo info) {
-    this.thirstManager.writeNbt(tag);
-  }
-
-  @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;addExhaustion(F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;addExhaustion(F)V", shift = Shift.AFTER))
-  private void addExhaustionMixin(float exhaustion, CallbackInfo info) {
-    if (!ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
-      this.thirstManager.addDehydration(exhaustion / ConfigInit.CONFIG.hydrating_factor);
-    }
-  }
-
-  @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;wakeUp(ZZ)V", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;sleepTimer:I"))
-  private void wakeUpMixin(boolean bl, boolean updateSleepingPlayers, CallbackInfo info) {
-    if (!this.world.isClient && !ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())
-        && this.sleepTimer >= 100) {
-      int thirstLevel = this.thirstManager.getThirstLevel();
-      int hungerLevel = this.hungerManager.getFoodLevel();
-      int thirstConsumption = ConfigInit.CONFIG.sleep_thirst_consumption;
-      int hungerConsumption = ConfigInit.CONFIG.sleep_hunger_consumption;
-      this.thirstManager.setThirstLevel(thirstLevel >= thirstConsumption ? thirstLevel - thirstConsumption : 0);
-      this.hungerManager.setFoodLevel(hungerLevel >= hungerConsumption ? hungerLevel - hungerConsumption : 0);
+    @Override
+    public ThirstManager getThirstManager(PlayerEntity player) {
+        return this.thirstManager;
     }
 
-  }
+    @Shadow
+    protected HungerManager hungerManager = new HungerManager();
+    @Shadow
+    private int sleepTimer;
+
+    public PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;update(Lnet/minecraft/entity/player/PlayerEntity;)V", shift = Shift.AFTER))
+    private void tickMixin(CallbackInfo info) {
+        if (!ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
+            this.thirstManager.update((PlayerEntity) (Object) this);
+        }
+    }
+
+    @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;tickMovement()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;updateItems()V", shift = Shift.BEFORE))
+    private void tickMovementMixin(CallbackInfo info) {
+        if (this.world.getDifficulty() == Difficulty.PEACEFUL && this.world.getGameRules().getBoolean(GameRules.NATURAL_REGENERATION)
+                && !ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
+            PlayerEntity player = (PlayerEntity) (Object) this;
+            this.thirstManager.update(player);
+            if (this.thirstManager.isNotFull() && this.age % 10 == 0) {
+                this.thirstManager.setThirstLevel(this.thirstManager.getThirstLevel() + 1);
+            }
+        }
+    }
+
+    @Inject(method = "readCustomDataFromNbt", at = @At(value = "TAIL"))
+    private void readCustomDataFromTagMixin(NbtCompound tag, CallbackInfo info) {
+        this.thirstManager.readNbt(tag);
+    }
+
+    @Inject(method = "writeCustomDataToNbt", at = @At(value = "TAIL"))
+    private void writeCustomDataToTagMixin(NbtCompound tag, CallbackInfo info) {
+        this.thirstManager.writeNbt(tag);
+    }
+
+    @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;addExhaustion(F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;addExhaustion(F)V", shift = Shift.AFTER))
+    private void addExhaustionMixin(float exhaustion, CallbackInfo info) {
+        if (!ConfigInit.CONFIG.excluded_names.contains(this.getName().asString())) {
+            if (ConfigInit.CONFIG.harder_nether && this.world.getRegistryKey() == World.NETHER) {
+                exhaustion *= ConfigInit.CONFIG.nether_factor;
+            }
+            this.thirstManager.addDehydration(exhaustion / ConfigInit.CONFIG.hydrating_factor);
+        }
+    }
+
+    @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;wakeUp(ZZ)V", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;sleepTimer:I"))
+    private void wakeUpMixin(boolean bl, boolean updateSleepingPlayers, CallbackInfo info) {
+        if (!this.world.isClient && !ConfigInit.CONFIG.excluded_names.contains(this.getName().asString()) && this.sleepTimer >= 100) {
+            int thirstLevel = this.thirstManager.getThirstLevel();
+            int hungerLevel = this.hungerManager.getFoodLevel();
+            int thirstConsumption = ConfigInit.CONFIG.sleep_thirst_consumption;
+            int hungerConsumption = ConfigInit.CONFIG.sleep_hunger_consumption;
+            this.thirstManager.setThirstLevel(thirstLevel >= thirstConsumption ? thirstLevel - thirstConsumption : 0);
+            this.hungerManager.setFoodLevel(hungerLevel >= hungerConsumption ? hungerLevel - hungerConsumption : 0);
+        }
+
+    }
 
 }
