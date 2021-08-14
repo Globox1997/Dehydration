d828116cc25749e40bffaa39dfa9531e17598775
RedGrapefruit <karpovanton729@gmail.com>
2021-08-14 15:14:09 +0300

    Add FlaskItem

---
+++ src/main/java/net/dehydration/api/FlaskItem.java
@@ -0,0 +1,12 @@
+package net.dehydration.api;
+
+import net.dehydration.item.Leather_Flask;
+
+/**
+ * A renamed {@link Leather_Flask}
+ */
+public class FlaskItem extends Leather_Flask {
+    public FlaskItem(int waterAddition, Settings settings) {
+        super(waterAddition, settings);
+    }
+}
