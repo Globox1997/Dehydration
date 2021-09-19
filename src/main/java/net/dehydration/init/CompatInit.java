3105952c8b244850da9c734dbdc46c858557a6f9
ParaJay <calumjhunter@gmail.com>
2021-09-19 15:27:32 +0100

    checks for origins

--- src/main/java/net/dehydration/init/CompatInit.java
+++ src/main/java/net/dehydration/init/CompatInit.java
@@ -4,9 +4,13 @@ import net.dehydration.compat.CroptopiaCompat;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class CompatInit {
+    
+    public static boolean origins_enabled;
 
     public static void init() {
         ifLoaded("croptopia", CroptopiaCompat::init);
+        
+        origins_enabled = (FabricLoader.getInstance().isModLoaded("origins"));
     }
 
     private static void ifLoaded(String mod, Action action) {
