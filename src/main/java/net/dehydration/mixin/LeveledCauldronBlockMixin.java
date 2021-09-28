a8f61a784bb3a0968b28de093cfd64f528fcc9a8
Globox1997 <mitschke11@web.de>
2021-09-28 20:18:05 +0200

    Fixed cauldron crash

--- src/main/java/net/dehydration/mixin/LeveledCauldronBlockMixin.java
+++ src/main/java/net/dehydration/mixin/LeveledCauldronBlockMixin.java
@@ -1,16 +1,18 @@
 package net.dehydration.mixin;
 
-import net.minecraft.block.LeveledCauldronBlock;
+import java.util.Map;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.At;
 
 import net.dehydration.init.SoundInit;
 import net.dehydration.item.Leather_Flask;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.AbstractCauldronBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LeveledCauldronBlock;
+import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundCategory;
@@ -21,13 +23,17 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-@Mixin(AbstractCauldronBlock.class)
-public class CauldronBlockMixin {
+@Mixin(LeveledCauldronBlock.class)
+public abstract class LeveledCauldronBlockMixin extends AbstractCauldronBlock {
 
-    @Inject(method = "onUse", at = @At(value = "HEAD"), cancellable = true)
-    public void onUseMixin(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> info) {
+    public LeveledCauldronBlockMixin(Settings settings, Map<Item, CauldronBehavior> behaviorMap) {
+        super(settings, behaviorMap);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         ItemStack itemStack = player.getStackInHand(hand);
-        if (itemStack.getItem() instanceof Leather_Flask) {
+        if (itemStack.getItem() instanceof Leather_Flask && state.getBlock().equals(Blocks.WATER_CAULDRON)) {
             NbtCompound tags = itemStack.getNbt();
             Leather_Flask item = (Leather_Flask) itemStack.getItem();
             if (itemStack.hasNbt() && tags.getInt("leather_flask") < 2 + item.addition) {
@@ -39,9 +45,9 @@ public class CauldronBlockMixin {
                     tags.putBoolean("purified_water", false);
                     itemStack.setNbt(tags);
                 }
-                info.setReturnValue(ActionResult.success(world.isClient));
+                return ActionResult.success(world.isClient);
             }
         }
+        return super.onUse(state, world, pos, player, hand, hit);
     }
-
 }
