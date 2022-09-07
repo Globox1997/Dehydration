f28a25a1d15736d30cc7eeaa6d6a856c4db323c6
Globox1997 <mitschke11@web.de>
2022-09-07 09:18:20 +0200

    moved to event

--- src/main/java/net/dehydration/init/EventInit.java
+++ src/main/java/net/dehydration/init/EventInit.java
@@ -6,11 +6,18 @@ import net.dehydration.access.ThirstManagerAccess;
 import net.dehydration.thirst.ThirstManager;
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.FluidTags;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
@@ -20,6 +27,13 @@ public class EventInit {
     public static void init() {
         ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> {
             ((ServerPlayerAccess) player).compatSync();
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
+            if (id.equals(new Identifier(LootTables.SPAWN_BONUS_CHEST.toString()))) {
+                LootPool pool = LootPool.builder().with(ItemEntry.builder(Items.GLASS_BOTTLE).build()).rolls(BinomialLootNumberProvider.create(5, 0.9F)).build();
+                supplier.pool(pool);
+            }
         });
 
         UseBlockCallback.EVENT.register((player, world, hand, result) -> {
