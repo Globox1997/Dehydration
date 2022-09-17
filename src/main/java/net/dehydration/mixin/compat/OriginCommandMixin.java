faa62fb7fda28df9958ad58bab36756b560ecd5a
Globox1997 <mitschke11@web.de>
2022-09-17 15:08:49 +0200

    updated origin compat

--- src/main/java/net/dehydration/mixin/compat/OriginCommandMixin.java
+++ src/main/java/net/dehydration/mixin/compat/OriginCommandMixin.java
@@ -1,8 +1,15 @@
 package net.dehydration.mixin.compat;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.spongepowered.asm.mixin.injection.At;
 
 import io.github.apace100.apoli.power.PowerTypeReference;
@@ -13,22 +20,23 @@ import io.github.apace100.origins.origin.OriginLayer;
 import net.dehydration.access.ThirstManagerAccess;
 import net.dehydration.network.ThirstServerPacket;
 import net.dehydration.thirst.ThirstManager;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+@SuppressWarnings("rawtypes")
 @Mixin(OriginCommand.class)
 public class OriginCommandMixin {
 
-    @Inject(method = "setOrigin", at = @At("TAIL"), remap = false)
-    private static void setOriginMixin(PlayerEntity player, OriginLayer layer, Origin origin, CallbackInfo info) {
-        ThirstManager thirstManager = ((ThirstManagerAccess) player).getThirstManager();
+    @Inject(method = "setOrigin", at = @At(value = "INVOKE", target = "Lio/github/apace100/origins/component/OriginComponent;partialOnChosen(Lnet/minecraft/entity/player/PlayerEntity;ZLio/github/apace100/origins/origin/Origin;)V"), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
+    private static void setOriginMixin(CommandContext<ServerCommandSource> commandContext, CallbackInfoReturnable<Integer> info, Collection targets, OriginLayer originLayer, Origin origin,
+            ServerCommandSource serverCommandSource, int processedTargets, Iterator var6, ServerPlayerEntity target) throws CommandSyntaxException {
+        ThirstManager thirstManager = ((ThirstManagerAccess) target).getThirstManager();
         boolean setThirst = true;
         if (origin.hasPowerType(new PowerTypeReference<>(Origins.identifier("fire_immunity"))) && thirstManager.hasThirst())
             setThirst = false;
 
         thirstManager.setThirst(setThirst);
-        if (player instanceof ServerPlayerEntity)
-            ThirstServerPacket.writeS2CExcludedSyncPacket((ServerPlayerEntity) player, setThirst);
+        ThirstServerPacket.writeS2CExcludedSyncPacket(target, setThirst);
 
     }
 }
