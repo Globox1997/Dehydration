2d40924e5b545dd0f9b0dabe9e7a3583765ba9a6
Globox1997 <mitschke11@web.de>
2022-09-17 15:08:22 +0200

    added null checks

--- src/main/java/net/dehydration/init/RenderInit.java
+++ src/main/java/net/dehydration/init/RenderInit.java
@@ -19,7 +19,8 @@ public class RenderInit {
     public static final Identifier THIRST_ICON = new Identifier("dehydration:textures/gui/thirst.png");
 
     public static void init() {
-        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> isPurifiedWater(world, pos, state) ? 3708358 : BiomeColors.getWaterColor(world, pos),
+        ColorProviderRegistry.BLOCK.register(
+                (state, world, pos, tintIndex) -> world != null && world.getBlockEntity(pos) != null ? isPurifiedWater(world, pos, state) ? 3708358 : BiomeColors.getWaterColor(world, pos) : 3708358,
                 BlockInit.CAMPFIRE_CAULDRON_BLOCK);
         ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> BiomeColors.getWaterColor(world, pos), BlockInit.COPPER_WATER_CAULDRON_BLOCK);
         ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> 3708358, BlockInit.COPPER_PURIFIED_WATER_CAULDRON_BLOCK);
