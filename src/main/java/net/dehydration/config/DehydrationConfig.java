28a7a505e6b3145cec1a119d92fa8623966e30e2
Globox1997 <mitschke11@web.de>
2022-10-10 12:01:23 +0200

    added source block setting

--- src/main/java/net/dehydration/config/DehydrationConfig.java
+++ src/main/java/net/dehydration/config/DehydrationConfig.java
@@ -65,6 +65,7 @@ public class DehydrationConfig implements ConfigData {
     public int pump_cooldown = 0;
     @Comment("Pump requires water below 10 blocks within 50 blocks")
     public boolean pump_requires_water = false;
+    public boolean bottle_consumes_source_block = false;
 
     public boolean harder_nether = false;
     @Comment("1.3 = 30% more thirst")
---
+++ src/main/java/net/dehydration/mixin/GlassBottleItemMixin.java
@@ -0,0 +1,32 @@
+package net.dehydration.mixin;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.dehydration.init.ConfigInit;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.AreaEffectCloudEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.GlassBottleItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@Mixin(GlassBottleItem.class)
+public class GlassBottleItemMixin {
+
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/player/PlayerEntity;DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void useMixin(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> info, List<AreaEffectCloudEntity> list, ItemStack itemStack,
+            HitResult hitResult, BlockPos blockPos) {
+        if (!world.isClient && ConfigInit.CONFIG.bottle_consumes_source_block)
+            world.setBlockState(blockPos, Blocks.AIR.getDefaultState());
+    }
+}
