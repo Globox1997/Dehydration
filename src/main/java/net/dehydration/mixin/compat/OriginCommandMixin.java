6e2903ff0be2a1ceb98588048bbed3a9b3782dd2
Globox1997 <mitschke11@web.de>
2023-04-04 14:01:36 +0200

    disabled origin compat

--- src/main/java/net/dehydration/mixin/compat/OriginCommandMixin.java
+++ src/main/java/net/dehydration/mixin/compat/OriginCommandMixin.java
@@ -1,42 +1,42 @@
-package net.dehydration.mixin.compat;
+// package net.dehydration.mixin.compat;
 
-import java.util.Collection;
-import java.util.Iterator;
+// import java.util.Collection;
+// import java.util.Iterator;
 
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
+// import com.mojang.brigadier.context.CommandContext;
+// import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+// import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+// import org.spongepowered.asm.mixin.injection.At;
 
-import io.github.apace100.apoli.power.PowerTypeReference;
-import io.github.apace100.origins.Origins;
-import io.github.apace100.origins.command.OriginCommand;
-import io.github.apace100.origins.origin.Origin;
-import io.github.apace100.origins.origin.OriginLayer;
-import net.dehydration.access.ThirstManagerAccess;
-import net.dehydration.network.ThirstServerPacket;
-import net.dehydration.thirst.ThirstManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
+// import io.github.apace100.apoli.power.PowerTypeReference;
+// import io.github.apace100.origins.Origins;
+// import io.github.apace100.origins.command.OriginCommand;
+// import io.github.apace100.origins.origin.Origin;
+// import io.github.apace100.origins.origin.OriginLayer;
+// import net.dehydration.access.ThirstManagerAccess;
+// import net.dehydration.network.ThirstServerPacket;
+// import net.dehydration.thirst.ThirstManager;
+// import net.minecraft.server.command.ServerCommandSource;
+// import net.minecraft.server.network.ServerPlayerEntity;
 
-@SuppressWarnings("rawtypes")
-@Mixin(OriginCommand.class)
-public class OriginCommandMixin {
+// @SuppressWarnings("rawtypes")
+// @Mixin(OriginCommand.class)
+// public class OriginCommandMixin {
 
-    @Inject(method = "setOrigin", at = @At(value = "INVOKE", target = "Lio/github/apace100/origins/component/OriginComponent;sync()V"), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
-    private static void setOriginMixin(CommandContext<ServerCommandSource> commandContext, CallbackInfoReturnable<Integer> info, Collection targets, OriginLayer originLayer, Origin origin,
-            ServerCommandSource serverCommandSource, int processedTargets, Iterator var6, ServerPlayerEntity target) throws CommandSyntaxException {
-        ThirstManager thirstManager = ((ThirstManagerAccess) target).getThirstManager();
-        boolean setThirst = true;
-        if (origin.hasPowerType(new PowerTypeReference<>(Origins.identifier("fire_immunity"))) && thirstManager.hasThirst())
-            setThirst = false;
+//     @Inject(method = "setOrigin", at = @At(value = "INVOKE", target = "Lio/github/apace100/origins/component/OriginComponent;sync()V"), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
+//     private static void setOriginMixin(CommandContext<ServerCommandSource> commandContext, CallbackInfoReturnable<Integer> info, Collection targets, OriginLayer originLayer, Origin origin,
+//             ServerCommandSource serverCommandSource, int processedTargets, Iterator var6, ServerPlayerEntity target) throws CommandSyntaxException {
+//         ThirstManager thirstManager = ((ThirstManagerAccess) target).getThirstManager();
+//         boolean setThirst = true;
+//         if (origin.hasPowerType(new PowerTypeReference<>(Origins.identifier("fire_immunity"))) && thirstManager.hasThirst())
+//             setThirst = false;
 
-        thirstManager.setThirst(setThirst);
-        ThirstServerPacket.writeS2CExcludedSyncPacket(target, setThirst);
+//         thirstManager.setThirst(setThirst);
+//         ThirstServerPacket.writeS2CExcludedSyncPacket(target, setThirst);
 
-    }
-}
+//     }
+// }
--- src/main/java/net/dehydration/mixin/compat/PlayerOriginComponentMixin.java
+++ src/main/java/net/dehydration/mixin/compat/PlayerOriginComponentMixin.java
@@ -1,41 +1,41 @@
-package net.dehydration.mixin.compat;
+// package net.dehydration.mixin.compat;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.Shadow;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+// import org.spongepowered.asm.mixin.injection.At;
 
-import io.github.apace100.apoli.power.PowerTypeReference;
-import io.github.apace100.origins.Origins;
-import io.github.apace100.origins.component.PlayerOriginComponent;
-import io.github.apace100.origins.origin.Origin;
-import io.github.apace100.origins.origin.OriginLayer;
-import net.dehydration.access.ServerPlayerAccess;
-import net.dehydration.access.ThirstManagerAccess;
-import net.dehydration.network.ThirstServerPacket;
-import net.dehydration.thirst.ThirstManager;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
+// import io.github.apace100.apoli.power.PowerTypeReference;
+// import io.github.apace100.origins.Origins;
+// import io.github.apace100.origins.component.PlayerOriginComponent;
+// import io.github.apace100.origins.origin.Origin;
+// import io.github.apace100.origins.origin.OriginLayer;
+// import net.dehydration.access.ServerPlayerAccess;
+// import net.dehydration.access.ThirstManagerAccess;
+// import net.dehydration.network.ThirstServerPacket;
+// import net.dehydration.thirst.ThirstManager;
+// import net.minecraft.entity.player.PlayerEntity;
+// import net.minecraft.server.network.ServerPlayerEntity;
 
-@Mixin(PlayerOriginComponent.class)
-public class PlayerOriginComponentMixin {
+// @Mixin(PlayerOriginComponent.class)
+// public class PlayerOriginComponentMixin {
 
-    @Shadow
-    private PlayerEntity player;
+//     @Shadow
+//     private PlayerEntity player;
 
-    @Inject(method = "setOrigin", at = @At("TAIL"), remap = false)
-    private void setOriginMixin(OriginLayer layer, Origin origin, CallbackInfo info) {
-        ThirstManager thirstManager = ((ThirstManagerAccess) player).getThirstManager();
-        boolean setThirst = true;
-        if (origin.hasPowerType(new PowerTypeReference<>(Origins.identifier("fire_immunity"))) && thirstManager.hasThirst())
-            setThirst = false;
+//     @Inject(method = "setOrigin", at = @At("TAIL"), remap = false)
+//     private void setOriginMixin(OriginLayer layer, Origin origin, CallbackInfo info) {
+//         ThirstManager thirstManager = ((ThirstManagerAccess) player).getThirstManager();
+//         boolean setThirst = true;
+//         if (origin.hasPowerType(new PowerTypeReference<>(Origins.identifier("fire_immunity"))) && thirstManager.hasThirst())
+//             setThirst = false;
 
-        thirstManager.setThirst(setThirst);
-        if (player instanceof ServerPlayerEntity) {
-            ThirstServerPacket.writeS2CExcludedSyncPacket((ServerPlayerEntity) player, setThirst);
-            ((ServerPlayerAccess) player).compatSync();
-        }
-    }
+//         thirstManager.setThirst(setThirst);
+//         if (player instanceof ServerPlayerEntity) {
+//             ThirstServerPacket.writeS2CExcludedSyncPacket((ServerPlayerEntity) player, setThirst);
+//             ((ServerPlayerAccess) player).compatSync();
+//         }
+//     }
 
-}
+// }
