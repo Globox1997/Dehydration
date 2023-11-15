25336a68be08e440d2813651fd2e0b6a2c6c210e
Globox1997 <mitschke11@web.de>
2023-11-15 11:50:36 +0100

    added create check

--- src/main/java/net/dehydration/mixin/WaterFluidMixin.java
+++ src/main/java/net/dehydration/mixin/WaterFluidMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.dehydration.fluid.PurifiedWaterFluid;
 import net.dehydration.init.FluidInit;
 import net.dehydration.init.TagInit;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FluidBlock;
@@ -22,17 +23,21 @@ import net.minecraft.world.WorldAccess;
 @Mixin(WaterFluid.class)
 public abstract class WaterFluidMixin extends FlowableFluid {
 
+    private static final boolean isCreateLoaded = FabricLoader.getInstance().isModLoaded("create");
+
     @Override
     protected void flow(WorldAccess world, BlockPos pos, BlockState state, Direction direction, FluidState fluidState) {
-        if (state.getFluidState().isIn(TagInit.PURIFIED_WATER))
+        if (state.getFluidState().isIn(TagInit.PURIFIED_WATER)) {
             world.setBlockState(pos, fluidState.getBlockState().with(FluidBlock.LEVEL, PurifiedWaterFluid.getBlockStateLevel(state.getFluidState())), Block.NOTIFY_ALL);
-        else
+        } else {
             super.flow(world, pos, state, direction, fluidState);
+        }
     }
 
     @Inject(method = "matchesType", at = @At("HEAD"), cancellable = true)
     private void matchesTypeMixin(Fluid fluid, CallbackInfoReturnable<Boolean> info) {
-        if (fluid == FluidInit.PURIFIED_WATER || fluid == FluidInit.PURIFIED_FLOWING_WATER)
+        if (!isCreateLoaded && (fluid == FluidInit.PURIFIED_WATER || fluid == FluidInit.PURIFIED_FLOWING_WATER)) {
             info.setReturnValue(true);
+        }
     }
 }
